3718060c6291c9e857b5231fba535a2c7e464d87
Adiithya178121 <adithya178121@gmail.com>
2025-03-30 00:03:23 +0530

    Create Simulating_a_Loading_Screen_with_setInterval.js

---
+++ Simulating_a_Loading_Screen_with_setInterval.js
@@ -0,0 +1,10 @@
+let counter = 0;
+const loadingInterval = setInterval(() => {
+    console.log("Loading...");
+    counter += 1;
+    
+    if (counter >= 5) {
+        clearInterval(loadingInterval);
+        console.log("Loaded successfully!");
+    }
+}, 1000);
